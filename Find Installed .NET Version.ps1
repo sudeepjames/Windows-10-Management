2ff5de031dd31f221c854ed01589b04c33cbe2ab
Sudeep James <sudeepjames@users.noreply.github.com>
2020-07-01 04:54:45 +0200

    Create Find Installed .NET Version.ps1

---
+++ Find Installed .NET Version.ps1
@@ -0,0 +1,1 @@
+Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | Get-ItemProperty -Name version -EA 0 | Where { $_.PSChildName -Match '^(?!S)\p{L}'} | Select PSChildName, version
